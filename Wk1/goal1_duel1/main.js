fd5cb5a35aacc950726a40e7bcc2c5d6fb513a95
Naxus28 <gabrielferraz@Gabriel-Ferrazs-MacBook-Pro.local>
2013-10-01 03:33:21 -0400

    Game1
    file created

---
+++ Wk1/goal1_duel1/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: gabrielferraz
+ * Date: 10/1/13
+ * Time: 3:23 AM
+ * To change this template use File | Settings | File Templates.
+ */
+//Gabriel Ferraz Id# 4265628
+
+       console.log("started");
