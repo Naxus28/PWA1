b649fe20e43422e309410ee27ad8f6ca71192309
Naxus28 <gabrielferraz@Gabriel-Ferrazs-MacBook-Pro.local>
2013-10-03 01:58:46 -0400

    Game1
    About to finish building the first function

--- Wk1/goal1_duel1/main.js
+++ Wk1/goal1_duel1/main.js
@@ -7,4 +7,62 @@
  */
 //Gabriel Ferraz Id# 4265628
 
-       console.log("started");
+      // console.log("started");
+
+    //Game instructions
+/*
+Damage occurs to both players at a random amount between half damage and maximum damage.
+So, if the player's damage variable is 50 then the amount of damage that can be inflicted will be between 25-50.
+•	Display the correct winner after the 10th round.
+If both players happen to die during the same round then display “No Winner Message”.
+•	Minimum of two functions.
+1.	fight() function - This function contains the code that loops through
+rounds and reduces the player’s health accordingly.
+2.	winnerCheck() function - This function is invoked after each round and returns
+a string of either the winner, the loser, a tie (both die) or no winner yet.
+This function does the conditional logic to determine if there is a winner or not.
+•	A break command is used to escape out of the loop if the fight is over before the 10 rounds is reached
+**/
+
+
+
+
+
+//damage should be something in between maximum and half of maximum. this means it is a random between half random and maximum random
+
+
+alert(playerOneName+":"+ playerOneHealth- +"**START**"+ playerTwoName +":" + playerTwoHealth);
+
+
+
+
+
+
+
+function fight(){
+
+    for(var i=0;i<10;i++) {
+
+    alert(playerOneName+":"+ playerOneHealth-finalDamageOne +"ROUND " + i + playerTwoName +":" + playerTwoHealth-finalDamageTwo);
+
+
+    var minDamageOne = playerOneDamage/2;
+    var minDamageTwo = playerTwoDamage/2;
+
+    var finalDamageOne = Math.floor((Math.random()*(playerOneDamage-minDamageOne)+minDamageOne));
+    var finalDamageTwo = Math.floor((Math.random()*(playerOneDamage-minDamageOne)+minDamageTwo));
+
+
+}
+}
+
+
+function winnerCheck(){
+
+
+
+
+
+}
+
+fight();
