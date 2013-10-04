e0e520601948f22207038a8ca621b5a7b7481541
Naxus28 <gabrielferraz@Gabriel-Ferrazs-MacBook-Pro.local>
2013-10-04 13:02:10 -0400

    function wordCount
    I did this one using a string method and a string property: split and
    length
    
    it is working well

--- Wk1/goal2_assign_jspractice/js/initial.js
+++ Wk1/goal2_assign_jspractice/js/initial.js
@@ -1,6 +1,6 @@
 
 //
-//create a function named 'avgNumbers'
+//1. create a function named 'avgNumbers'
 //    - accept 1 parameter into the function that will be an array of unlimited numbers
 //- find the average of all the numbers
 //- return the average from the function
@@ -28,7 +28,7 @@ function avgNumbers(array){
 console.log('avg number = ', avgNumbers([1,2,3,4,5,6,7]));
 
 
-//THE CODE BELOW IS A LITTLE TEST TO SEE IF I KNOW HOW TO CALL THE FUNCTION ABOVE - IT TAKES TWO PARAMETERS AND
+//1.a. THE CODE BELOW IS A LITTLE TEST TO SEE IF I KNOW HOW TO CALL THE FUNCTION ABOVE - IT TAKES TWO PARAMETERS AND
 // CALCULATES THE SUM OF THE AVERAGE OF THESE PARAMETERS AND THE RESULT THE FUNCTION ABOVE (THE VALUE OF THE PARAMETERS WILL CHANGE)
 
 function sumAvgNumbers(a,b){
@@ -39,7 +39,7 @@ function sumAvgNumbers(a,b){
 console.log("The sum of the two averages is "+ sumAvgNumbers(5,5));
 
 
-//THIS IS A THIRD FUNCTION CALLING THE FUNCTION ABOVE - IT IS GOING TO MULTIPLY THE RESULT OF THE ABOVE FUNCTIONS BY
+//1.b.THIS IS A THIRD FUNCTION CALLING THE FUNCTION ABOVE - IT IS GOING TO MULTIPLY THE RESULT OF THE ABOVE FUNCTIONS BY
 //WHATEVER NUMBER IS INPUT
 
 function resultOfFunctions(a,functionOne,functionTwo){
@@ -51,7 +51,7 @@ console.log("The value resulting from the multiplication of the three functions 
 
 //
 
-//create a function named 'fullName'
+//2. create a function named 'fullName'
 //    - accept 2 parameters into the function that are strings (firstname and lastname)
 //- return the name after it has been concatenated
 //- console.log the answer outside of the function
@@ -61,5 +61,21 @@ function fullName(firstname,lastname){
     return   firstname +" "+ lastname
 }
 
-console.log(fullName("Gabriel,"Ferraz"));
+console.log(fullName("Gabriel","Ferraz"));
+
+
+//3. create a function named 'wordCount'
+//    - accept 1 parameter into the function that is a long string of text words
+//- create a function that counts all the words and return the answer
+//- console.log the answer outside of the function
+
+
+function wordCount(sentence){
+
+
+    return  sentence.split(" ").length
+
+}
+
+console.log("I am writing a sentence that has " + wordCount("I am writing a sentence that has") + " words");
 
