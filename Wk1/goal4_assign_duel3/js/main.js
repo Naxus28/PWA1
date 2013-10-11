b1f423aeb74650cdf9ad6d81605a39d595c1bf15
Naxus28 <gabrielferraz@Gabriel-Ferrazs-MacBook-Pro.local>
2013-10-11 10:35:55 -0400

    Game3.12 - changed the position of the code that disable the butto. Moved it up to the top of the function

--- Wk1/goal4_assign_duel3/js/main.js
+++ Wk1/goal4_assign_duel3/js/main.js
@@ -16,48 +16,10 @@
  */
 //Gabriel Ferraz Id# 4265628
 
-// console.log("started");
-
-//Game instructions
-/*
- Damage occurs to both players at a random amount between half damage and maximum damage.
- So, if the player's damage variable is 50 then the amount of damage that can be inflicted will be between 25-50.
- •	Display the correct winner after the 10th round.
- If both players happen to die during the same round then display “No Winner Message”.
- •	Minimum of two functions.
- 1.	fight() function - This function contains the code that loops through
- rounds and reduces the player’s health accordingly.
- 2.	winnerCheck() function - This function is invoked after each round and returns
- a string of either the winner, the loser, a tie (both die) or no winner yet.
- This function does the conditional logic to determine if there is a winner or not.
- •	A break command is used to escape out of the loop if the fight is over before the 10 rounds is reached
- **/
-
-
-//variables
-//player one
-
-
-
-//damage should be something in between maximum and half of maximum. this means it is a random between half random and maximum random
-
-
 
 /*
 
- To obtain full credit on the assignment your submission should match the functionality of the demonstration. The following criteria must be adhered to as well as satisfying all items on this assignment's rubric.
- Open the js/main.js file.
- All uses of alert() must be removed
-
- An object with three properties(keys) is created for both fighters. The three properties are:
- name
- damage
- health
- Example: fighter1 = {name:Spiderman, damage:20, health:100};
- Both fighter objects should be in an array
- THIS TASK ABOVE IS DONE
-
-
+ASSIGNMENT - GAME 3
 
  Modify fight() function
  Since this assignment is now using objects, the code in the fight() function must be modified from the previous version of the assignment to accommodate for this. NO loop will be needed!-since clicking on the button is what triggers the next round.
@@ -74,6 +36,7 @@
  Fighter's name and health at the top
  Current round number above the button
  Create a click event on the button
+
 
  When the button is clicked, the following should commence:
  advance round
@@ -101,10 +64,18 @@
 //document.getElementById("kabal").style.textAlign="left";
 //document.getElementById("kratos").style.textAlign="right";
 
-var round=1;
+
 
 document.querySelector(".buttonblue").onclick = function(){
          (fight());
+    if(results==="NO WINNER"){
+
+
+        document.querySelector(".buttonblue").disabled=true;
+
+
+    }
+
      };
 
 document.getElementById("round").style.textAlign= "center";
@@ -114,9 +85,10 @@ document.getElementById("round").innerHTML = "**START**";
 document.getElementById("kabal").innerHTML = arrayFighters[0].name +"   " +arrayFighters[0].health;
 document.getElementById("kratos").innerHTML = arrayFighters[1].name +"   " + arrayFighters[1].health;
 
-
+var round=1;
 
 function fight(){
+
 
         var minDamageOne = arrayFighters[0].damage/2;
         var minDamageTwo = arrayFighters[1].damage/2;
@@ -151,11 +123,13 @@ function fight(){
                 document.getElementById("scores").style.textAlign="center";
                 document.getElementById("scores").innerHTML = "Game Over";
 
-                function disableButton(){
-                    document.querySelector(".buttonblue").disabled=true;
-                }
+//                function disableButton(){
+//                    document.querySelector(".buttonblue").disabled=true;
+//                }
+//
+//                document.querySelector(".buttonblue").onclick= disableButton();
 
-                document.querySelector(".buttonblue").onclick= disableButton();
+                console.log(arrayFighters[0].health);
 
 
             }
@@ -183,5 +157,5 @@ function fight(){
 
 
 
-    fight();
+   // fight();
 })();
